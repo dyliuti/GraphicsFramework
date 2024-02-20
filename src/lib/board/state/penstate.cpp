19fa53bf248ed563cdf3053f519201c6bb677d93
颜敏炜 <yanminwei1993@163.com>
2024-02-20 23:06:14 +0800

    edit pen state

--- src/lib/board/state/penstate.cpp
+++ src/lib/board/state/penstate.cpp
@@ -1,15 +1,23 @@
 ﻿#include "penstate.h"
+#include <QGraphicsItemGroup>
 
 BOARD_NAMESPACE_USE
+static std::shared_ptr<QGraphicsItemGroup> s_lineGroup;
+
 void PenState::mousePressEvent(QGraphicsSceneMouseEvent* event)
 {
 }
 
 void PenState::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
 {
+    if (!s_lineGroup) {
+        s_lineGroup = std::make_shared<QGraphicsItemGroup>();
+        s_lineGroup->setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable);
+    }
 }
 
 void PenState::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
 {
+    s_lineGroup = nullptr;
     //    switchState(kAnimationNormalStateKey)->mouseReleaseEvent(event);
 }
